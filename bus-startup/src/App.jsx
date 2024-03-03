e614f292d314034a7d49f110cde597f1004a89d1
EdrinM22 <emolla22@epoka.edu.al>
2024-03-03 21:54:04 +0100

    added buttons

--- bus-startup/src/App.jsx
+++ bus-startup/src/App.jsx
@@ -1,11 +1,20 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-
+import LoginButton from "./components/custom_elements/login_button"
+import JoinUsButton from "./components/custom_elements/join_us_button"
+import GoToMap from "./components/custom_elements/go_to_map.button"
+import SubmitButton from "./components/custom_elements/submit_button"
+import LogOutButton from "./components/custom_elements/logout_button"
 function App() {
   return (
     <>
       <Header />
       <Footer />
+      <LoginButton/>
+      <JoinUsButton/>
+      <GoToMap/>
+      <SubmitButton/>
+      <LogOutButton/>
     </>
   )
 }
